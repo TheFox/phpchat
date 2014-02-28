1fcb60c95996de5cc6416c27bce1845ee99b024f
Christian Mayer <thefox21at@gmail.com>
2014-02-28 17:35:42 +0100

    Yaml base.

---
+++ src/TheFox/Yaml/YamlStorage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TheFox\Yaml;
+
+use Symfony\Component\Yaml\Yaml;
+
+class YamlStorage{
+	
+	public function __construct(){
+		#print __CLASS__.'->'.__FUNCTION__.''."\n";
+		
+	}
+	
+}
