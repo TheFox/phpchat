9693ec9160b490f69d46fd0072c328e96650a167
Christian Mayer <thefox21at@gmail.com>
2014-03-04 12:48:37 +0100

    Signal handler.

--- kernel.php
+++ kernel.php
@@ -6,4 +6,22 @@ use TheFox\PhpChat\Kernel;
 
 
 $kernel = new Kernel();
+
+$log->info('signal handler setup');
+declare(ticks = 1);
+$exit = 0;
+if(function_exists('pcntl_signal')){
+	function signalHandler($signo){
+		global $exit, $log, $kernel;
+		$exit++;
+		print "\n";
+		$log->notice('main abort ['.$exit.']');
+		$kernel->setExit($exit);
+		if($exit >= 2)
+			exit();
+	}
+	pcntl_signal(SIGTERM, 'signalHandler');
+	pcntl_signal(SIGINT, 'signalHandler');
+}
+
 $kernel->run();
