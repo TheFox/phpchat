5c2fd4f3772b82eb8ac250ac85e019e86ca8d2c2
Christian Mayer <thefox21at@gmail.com>
2014-03-07 20:20:23 +0100

    Debug.

--- src/TheFox/Network/StreamSocket.php
+++ src/TheFox/Network/StreamSocket.php
@@ -87,7 +87,7 @@ class StreamSocket extends AbstractSocket{
 	public function write($data){
 		$rv = stream_socket_sendto($this->getHandle(), $data);
 		
-		print __CLASS__.'->'.__FUNCTION__.': '.$rv.', "'.substr($data, 0, -1).'"'."\n";
+		#print __CLASS__.'->'.__FUNCTION__.': '.$rv.', "'.substr($data, 0, -1).'"'."\n";
 		return $rv;
 	}
 	
