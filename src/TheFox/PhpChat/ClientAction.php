f19b00bfd22e1a6a307c90f784915d48f460f73d
Christian Mayer <thefox21at@gmail.com>
2014-03-07 22:02:15 +0100

    functionExec().

--- src/TheFox/PhpChat/ClientAction.php
+++ src/TheFox/PhpChat/ClientAction.php
@@ -4,6 +4,7 @@ namespace TheFox\PhpChat;
 
 use Exception;
 use RuntimeException;
+use Closure;
 
 class ClientAction{
 	
@@ -50,6 +51,42 @@ class ClientAction{
 		else{
 			$this->objc = $objc;
 			$this->func = $func;
+		}
+	}
+	
+	public function functionExec(){
+		print __CLASS__.'->'.__FUNCTION__.''."\n";
+		#print __CLASS__.'->'.__FUNCTION__.': inst '.(int)($this->func instanceof Closure)."\n";
+		
+		$args = func_get_args();
+		
+		$objc = $this->objc;
+		$func = $this->func;
+		
+		#print __CLASS__.'->'.__FUNCTION__.': objc '.gettype($objc).', '.get_class($objc)."\n";
+		#print __CLASS__.'->'.__FUNCTION__.': func '.gettype($func).', '.get_class($func)."\n";
+		
+		#print_r($objc);
+		#print_r($func);
+		
+		if($objc === null && $func === null){
+			#print __CLASS__.'->'.__FUNCTION__.': null'."\n";
+			return null;
+		}
+		elseif($objc === null && $func instanceof Closure){
+			#print __CLASS__.'->'.__FUNCTION__.': exec anon'."\n";
+			return call_user_func_array($func, $args);
+		}
+		elseif($objc === null && is_string($func)){
+			#print __CLASS__.'->'.__FUNCTION__.': exec string "'.$func.'"'."\n";
+			return call_user_func_array($func, $args);
+		}
+		elseif(is_object($objc) && is_string($func)){
+			#print __CLASS__.'->'.__FUNCTION__.': exec objc'."\n";
+			return call_user_func_array(array($objc, $func), $args);
+		}
+		else{
+			#print __CLASS__.'->'.__FUNCTION__.': else'."\n";
 		}
 	}
 	
