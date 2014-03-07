bc6714a02cf64e89972250654acd23b801a05aab
Christian Mayer <thefox21at@gmail.com>
2014-03-07 20:21:28 +0100

    Id.

--- src/TheFox/PhpChat/ClientAction.php
+++ src/TheFox/PhpChat/ClientAction.php
@@ -9,6 +9,8 @@ class ClientAction{
 	
 	const CRITERION_NONE = 0;
 	const CRITERION_AFTER_CONNECT = 1;
+	const CRITERION_AFTER_ID = 2;
+	const CRITERION_AFTER_ID_OK = 4;
 	
 	private $id = 0;
 	private $criteria = 0;
@@ -20,6 +22,10 @@ class ClientAction{
 	
 	public function setId($id){
 		$this->id = $id;
+	}
+	
+	public function getId(){
+		return $this->id;
 	}
 	
 	public function getCriteria(){
