5cfca3081c2b33186ac30dd015d855b7fc62b842
Christian Mayer <thefox21at@gmail.com>
2014-03-28 17:37:31 +0100

    Msg/Queue added.

---
+++ src/TheFox/PhpChat/Msg.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace TheFox\PhpChat;
+
+use Rhumsaa\Uuid\Uuid;
+use Rhumsaa\Uuid\Exception\UnsatisfiedDependencyException;
+
+class Msg{
+	
+	private $id = '';
+	private $srcNodeId = '';
+	private $srcUserNickname = '';
+	private $dstNodeId = '';
+	private $text = '';
+	private $timeCreated = 0;
+	
+	public function __construct($text = ''){
+		try{
+			$this->setId((string)Uuid::uuid4());
+		}
+		catch(UnsatisfiedDependencyException $e){
+			# TODO
+		}
+		
+		$this->setText($text);
+		$this->setTimeCreated(time());
+	}
+	
+	public function __sleep(){
+		#return array('id');
+	}
+	
+	public function setId($id){
+		$this->id = $id;
+	}
+	
+	public function getId(){
+		return $this->id;
+	}
+	
+	public function setSrcNodeId($srcNodeId){
+		$this->srcNodeId = $srcNodeId;
+	}
+	
+	public function getSrcNodeId(){
+		return $this->srcNodeId;
+	}
+	
+	public function setSrcUserNickname($srcUserNickname){
+		$this->srcUserNickname = $srcUserNickname;
+	}
+	
+	public function getSrcUserNickname(){
+		return $this->srcUserNickname;
+	}
+	
+	public function setDstNodeId($dstNodeId){
+		$this->dstNodeId = $dstNodeId;
+	}
+	
+	public function getDstNodeId(){
+		return $this->dstNodeId;
+	}
+	
+	public function setText($text){
+		$this->text = $text;
+	}
+	
+	public function getText(){
+		return $this->text;
+	}
+	
+	public function setTimeCreated($timeCreated){
+		$this->timeCreated = $timeCreated;
+	}
+	
+	public function getTimeCreated(){
+		return $this->timeCreated;
+	}
+	
+}
---
+++ src/TheFox/PhpChat/MsgSendQueue.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace TheFox\PhpChat;
+
+use TheFox\Yaml\YamlStorage;
+
+class MsgSendQueue extends YamlStorage{
+	
+	private $msgsId = 0;
+	private $msgs = array();
+	
+	public function __construct($filePath = null){
+		#print __CLASS__.'->'.__FUNCTION__.''."\n";
+		parent::__construct($filePath);
+		
+		$this->data['timeCreated'] = time();
+	}
+	
+	public function save(){
+		#print __CLASS__.'->'.__FUNCTION__.''."\n";
+		
+		$this->data['msgs'] = array();
+		foreach($this->msgs as $msgId => $msg){
+			
+			$msgAr = array();
+			$msgAr['id'] = $msg->getId();
+			$msgAr['srcNodeId'] = $msg->getSrcNodeId();
+			$msgAr['srcUserNickname'] = $msg->getSrcUserNickname();
+			$msgAr['dstNodeId'] = $msg->getDstNodeId();
+			$msgAr['timeCreated'] = $msg->getTimeCreated();
+			
+			$this->data['msgs'][$msgAr['id']] = $msgAr;
+		}
+		
+		$rv = parent::save();
+		unset($this->data['msgs']);
+		
+		return $rv;
+	}
+	
+	public function load(){
+		#print __CLASS__.'->'.__FUNCTION__.''."\n";
+		
+		if(parent::load()){
+			
+			if(isset($this->data['msgs']) && $this->data['msgs']){
+				foreach($this->data['msgs'] as $msgId => $msgAr){
+					
+					$msg = new Msg();
+					$msg->setId($msgAr['id']);
+					$msg->setSrcNodeId($msgAr['srcNodeId']);
+					$msg->setSrcUserNickname($msgAr['srcUserNickname']);
+					$msg->setDstNodeId($msgAr['dstNodeId']);
+					$msg->setTimeCreated($msgAr['timeCreated']);
+					
+					$this->msgs[$msg->getId()] = $msg;
+				}
+			}
+			unset($this->data['msgs']);
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public function msgAdd(Msg $msg){
+		$this->msgs[$msg->getId()] = $msg;
+		$this->setDataChanged(true);
+	}
+	
+}
