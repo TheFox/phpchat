9b90caf40e8f63f5210a512ea5e16fba0571ff26
Christian Mayer <thefox21at@gmail.com>
2014-03-31 16:01:44 +0200

    Msg encryption.

--- src/TheFox/PhpChat/Msg.php
+++ src/TheFox/PhpChat/Msg.php
@@ -12,10 +12,15 @@ class Msg{
 	private $srcSslKeyPub = '';
 	private $srcUserNickname = '';
 	private $dstNodeId = '';
+	private $dstSslPubKey = '';
 	private $text = '';
+	private $password = '';
+	private $checksum = '';
 	private $sentNodes = array();
 	private $relayCount = 0;
 	private $timeCreated = 0;
+	
+	private $ssl = null;
 	
 	public function __construct($text = ''){
 		try{
@@ -73,12 +78,36 @@ class Msg{
 		return $this->dstNodeId;
 	}
 	
+	public function setDstSslPubKey($dstSslPubKey){
+		$this->dstSslPubKey = $dstSslPubKey;
+	}
+	
+	private function getDstSslPubKey(){
+		return $this->dstSslPubKey;
+	}
+	
 	public function setText($text){
 		$this->text = $text;
 	}
 	
 	public function getText(){
 		return $this->text;
+	}
+	
+	public function setPassword($password){
+		$this->password = $password;
+	}
+	
+	public function getPassword(){
+		return $this->password;
+	}
+	
+	public function setChecksum($checksum){
+		$this->checksum = $checksum;
+	}
+	
+	public function getChecksum(){
+		return $this->checksum;
 	}
 	
 	public function setSentNodes($sentNodes){
@@ -107,6 +136,103 @@ class Msg{
 	
 	public function getTimeCreated(){
 		return((int)$this->timeCreated);
+	}
+	
+	
+	public function setSsl($ssl){
+		$this->ssl = $ssl;
+	}
+	
+	public function getSsl(){
+		return $this->ssl;
+	}
+	
+	public function setSslPrv($sslKeyPrvPath, $sslKeyPrvPass){
+		#$this->setSsl(openssl_pkey_get_private(file_get_contents($sslKeyPrvPath), $sslKeyPrvPass)); # TODO
+		$this->setSsl(openssl_pkey_get_private($sslKeyPrvPath, $sslKeyPrvPass));
+	}
+	
+	public function encrypt(){
+		print __CLASS__.'->'.__FUNCTION__.''."\n";
+		$rv = false;
+		
+		if(!$this->getSsl()){
+			throw new RuntimeException('ssl not set.', 1);
+		}
+		if(!$this->getDstSslPubKey()){
+			throw new RuntimeException('dstSslPubKey not set.', 2);
+		}
+		
+		$text = $this->text;
+		$password = hash('sha512', mt_rand(0, 999999).'_'.time());
+		$passwordEncrypted = '';
+		
+		if(openssl_sign($password, $sign, $this->getSsl(), OPENSSL_ALGO_SHA1)){
+			$sign = base64_encode($sign);
+			
+			if(openssl_public_encrypt($password, $cryped, $this->getSrcSslKeyPub())){
+				$passwordBase64 = base64_encode($cryped);
+				$jsonStr = json_encode(array('data' => $passwordBase64, 'sign' => $sign));
+				$gzdata = gzencode($jsonStr, 9);
+				$passwordEncrypted = base64_encode($gzdata);
+				
+				$this->setPassword($passwordEncrypted);
+			}
+			else{
+				throw new RuntimeException('openssl_public_encrypt failed.', 101);
+			}
+		}
+		else{
+			throw new RuntimeException('openssl_sign failed.', 102);
+		}
+		
+		print __CLASS__.'->'.__FUNCTION__.' password: '.$password."\n";
+		#print __CLASS__.'->'.__FUNCTION__.' passwordEncrypted: '.$passwordEncrypted."\n";
+		
+		if($passwordEncrypted){
+			if(openssl_sign($text, $sign, $this->getSsl(), OPENSSL_ALGO_SHA1)){
+				$sign = base64_encode($sign);
+				$textBase64 = base64_encode($text);
+				$userNickname = base64_encode($this->getSrcUserNickname());
+				
+				$jsonStr = json_encode(array(
+					'text' => $textBase64,
+					'sign' => $sign,
+					'userNickname' => $userNickname,
+				));
+				$data = gzencode($jsonStr, 9);
+				
+				$iv = substr(hash('sha512', mt_rand(0, 999999), true), 0, 16);
+				$data = openssl_encrypt($data, 'AES-256-CBC', $password, 0, $iv);
+				if($data !== false){
+					$iv = base64_encode($iv);
+					
+					$jsonStr = json_encode(array(
+						'data' => $data,
+						'iv' => $iv,
+					));
+					$data = gzencode($jsonStr, 9);
+					$data = base64_encode($data);
+					
+					$this->setText($data);
+					
+					$checksumData = $this->getId().'_'.$this->getSrcNodeId().'_'.$this->getDstNodeId().'_'.base64_encode($text).'_'.$this->getTimeCreated();
+					$this->setChecksum(hash_hmac('sha512', $checksumData, $password));
+					
+					$rv = true;
+				}
+			}
+		}
+		else{
+			throw new RuntimeException('Can\'t create password.', 103);
+		}
+		
+		print __CLASS__.'->'.__FUNCTION__.' text: '.$this->getText()."\n";
+		print __CLASS__.'->'.__FUNCTION__.' checksum: '.$this->getChecksum()."\n";
+		
+		print __CLASS__.'->'.__FUNCTION__.': done'."\n";
+		
+		return $rv;
 	}
 	
 }
