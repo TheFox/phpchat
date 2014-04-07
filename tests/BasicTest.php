b870dc28792733266df443449f40399c75304cdf
Christian Mayer <thefox21at@gmail.com>
2014-04-07 14:22:45 +0200

    Basic test.

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -1,8 +1,37 @@
 <?php
 
+use Rhumsaa\Uuid\Uuid;
+use Rhumsaa\Uuid\Exception\UnsatisfiedDependencyException;
+
 class BasicTest extends PHPUnit_Framework_TestCase{
 	
-	public function testPushAndPop(){
+	public function testStrIsUuid(){
+		
+		$id = '00000000-0000-4000-8000-000000000000';
+		$this->assertTrue(strIsUuid($id));
+		
+		$id = '00000000-0000-4000-8000-00000000000x';
+		$this->assertFalse(strIsUuid($id));
+		
+		$id = '00000000-0000-4000-8000-0000000000';
+		$this->assertFalse(strIsUuid($id));
+		
+		$id = '00000000-0000-0000-0000-000000000000';
+		$this->assertFalse(strIsUuid($id));
+		
+		$id = 'badfood0-0000-4000-a000-000000000000';
+		$this->assertFalse(strIsUuid($id));
+		
+		$id = 'cafed00d-2131-4159-8e11-0b4dbadb1738';
+		$this->assertTrue(strIsUuid($id));
+		
+		$id = (string)Uuid::uuid4();
+		$this->assertTrue(strIsUuid($id));
+		
+		return $id;
+	}
+	
+	/*public function testPushAndPop(){
 		$stack = array();
 		#$this->assertEquals(0, count($stack));
 		
@@ -16,7 +45,7 @@ class BasicTest extends PHPUnit_Framework_TestCase{
 		$this->assertTrue(TRUE, 'This should already work.');
 		
 		#$this->markTestSkipped('The MySQLi extension is not available.');
-	}
+	}*/
 	
 	/*public function testOne(){
 		#print __CLASS__.'->'.__FUNCTION__.''."\n";
