ee47f26b1ff884a0274aa62d4fac37eaf15b5e3d
Christian Mayer <thefox21at@gmail.com>
2014-05-01 02:43:51 +0200

    testFunctions() added.

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -31,4 +31,9 @@ class BasicTest extends PHPUnit_Framework_TestCase{
 		return $id;
 	}
 	
+	public function testFunctions(){
+		$this->assertTrue(in_array('sha512', hash_algos()), 'sha512 algorithm not found.');
+		$this->assertTrue(in_array('ripemd160', hash_algos()), 'ripemd160 algorithm not found.');
+	}
+	
 }
