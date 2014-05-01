814c2698b7c43a91887065e27161c80b05fac9c8
Christian Mayer <thefox21at@gmail.com>
2014-05-01 02:38:38 +0200

    Console tests added.

---
+++ tests/ConsoleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use TheFox\PhpChat\Console;
+
+class ConsoleTest extends PHPUnit_Framework_TestCase{
+	
+	public function testStrings(){
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+	
+}
