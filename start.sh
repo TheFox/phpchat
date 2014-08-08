ab258e9d3c12a07b0ef1ddc6936074113410df70
Christian Mayer <christian@fox21.at>
2014-08-08 16:14:30 +0200

    Sleep added.

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ SCRIPT_BASEDIR=$(dirname $0)
 cd $SCRIPT_BASEDIR
 
 ./application.php kernel -d
+sleep 1
 ./application.php cronjob -d
 ./application.php imap -d -a 127.0.0.1 -p 21143
 ./application.php smtp -d -a 127.0.0.1 -p 21025
