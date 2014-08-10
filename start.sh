d9388d5457305a0f714e2cc787f73ecf52207b4d
Christian Mayer <christian@fox21.at>
2014-08-10 16:44:40 +0200

    Order.

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ cd $SCRIPT_BASEDIR
 ./application.php kernel -d
 sleep 1
 ./application.php cronjob -d
-./application.php imap -d -a 127.0.0.1 -p 21143
-./application.php smtp -d -a 127.0.0.1 -p 21025
+./application.php imap -a 127.0.0.1 -p 21143 -d
+./application.php smtp -a 127.0.0.1 -p 21025 -d
 ./application.php console
