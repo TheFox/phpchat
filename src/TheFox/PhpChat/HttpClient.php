ff8d085f679763d4ab8b9187cb4a174dfc10e226
Christian Mayer <christian@fox21.at>
2014-08-18 10:48:17 +0200

    Send functions.

--- src/TheFox/PhpChat/HttpClient.php
+++ src/TheFox/PhpChat/HttpClient.php
@@ -42,6 +42,171 @@ class HttpClient extends Client{
 		fwrite(STDOUT, __CLASS__.'->'.__FUNCTION__."\n");
 	}
 	
+	public function sendHello(){
+		$data = array(
+			'ip' => $this->getUri()->getHost(),
+		);
+		return $this->dataSend($this->msgCreate('hello', $data));
+	}
+	
+	public function sendId(){
+		return '';
+	}
+	
+	private function sendIdOk(){
+		return '';
+	}
+	
+	public function sendNodeFind($nodeId, $distance = null, $nodesFoundIds = null, $useHashcash = true){
+		if(!$this->getTable()){
+			throw new RuntimeException('table not set.');
+		}
+		if($distance === null){
+			$distance = 'ffffffff-ffff-4fff-bfff-ffffffffffff';
+		}
+		if($nodesFoundIds === null){
+			$nodesFoundIds = array();
+		}
+		
+		$rid = (string)Uuid::uuid4();
+		
+		$this->requestAdd('node_find', $rid, array(
+			'nodeId' => $nodeId,
+			'distance' => $distance,
+			'nodesFoundIds' => $nodesFoundIds,
+		));
+		
+		$data = array(
+			'rid'       => $rid,
+			'num'       => static::NODE_FIND_NUM,
+			'nodeId'    => $nodeId,
+			'hashcash'  => '',
+		);
+		if($useHashcash){
+			$data['hashcash'] = $this->hashcashMint(static::HASHCASH_BITS_MIN);
+		}
+		return $this->dataSend($this->msgCreate('node_find', $data));
+	}
+	
+	public function sendNodeFound($rid, $nodes = array(), $useHashcash = true){
+		return '';
+	}
+	
+	public function sendMsg(Msg $msg){
+		$rid = (string)Uuid::uuid4();
+		
+		$this->requestAdd('msg', $rid, array(
+			'msg' => $msg,
+		));
+		
+		$data = array(
+			'rid' => $rid,
+			
+			'version' => $msg->getVersion(),
+			'id' => $msg->getId(),
+			'srcNodeId' => $msg->getSrcNodeId(),
+			'srcSslKeyPub' => base64_encode($msg->getSrcSslKeyPub()),
+			'srcUserNickname' => $msg->getSrcUserNickname(),
+			'dstNodeId' => $msg->getDstNodeId(),
+			'body' => $msg->getBody(),
+			'password' => $msg->getPassword(),
+			'checksum' => $msg->getChecksum(),
+			'relayCount' => (int)$msg->getRelayCount() + 1,
+			'timeCreated' => (int)$msg->getTimeCreated(),
+			'hashcash' => $this->hashcashMint(static::HASHCASH_BITS_MAX),
+		);
+		return $this->dataSend($this->msgCreate('msg', $data));
+	}
+	
+	private function sendMsgResponse($rid, $status){
+		return '';
+	}
+	
+	public function sendSslInit($useHashcash = true){
+		return '';
+	}
+	
+	private function sendSslInitResponse($code){
+		return '';
+	}
+	
+	private function sendSslTest(){
+		return '';
+	}
+	
+	private function sendSslVerify($token){
+		return '';
+	}
+	
+	private function sendSslPasswordPut(){
+		return '';
+	}
+	
+	private function sendSslPasswordReput(){
+		return '';
+	}
+	
+	private function sendSslPasswordTest(){
+		return '';
+	}
+	
+	private function sendSslPasswordRetest(){
+		return '';
+	}
+	
+	private function sendSslPasswordVerify($token){
+		return '';
+	}
+	
+	private function sendSslPasswordReverify($token){
+		return '';
+	}
+	
+	public function sendSslKeyPubGet($nodeSslKeyPubFingerprint){
+		return '';
+	}
+	
+	private function sendSslKeyPubPut($rid,
+		$nodeId = null, $nodeIp = null, $nodePort = null, $nodeSslKeyPubFingerprint = null, $nodeSslKeyPub = null){
+		return '';
+	}
+	
+	public function sendTalkRequest($userNickname){
+		return '';
+	}
+	
+	public function sendTalkResponse($rid, $status, $userNickname = ''){
+		return '';
+	}
+	
+	public function sendTalkMsg($rid, $userNickname, $text, $ignore){
+		return '';
+	}
+	
+	public function sendTalkUserNicknameChange($userNicknameOld, $userNicknameNew){
+		return '';
+	}
+	
+	public function sendTalkClose($rid, $userNickname){
+		return '';
+	}
+	
+	public function sendPing($id = ''){
+		return '';
+	}
+	
+	public function sendPong($id = ''){
+		return '';
+	}
+	
+	private function sendError($errorCode = 999, $msgName = ''){
+		return '';
+	}
+	
+	public function sendQuit(){
+		return '';
+	}
+	
 	public function shutdown(){
 		if(!$this->getStatus('hasShutdown')){
 			$this->setStatus('hasShutdown', true);
