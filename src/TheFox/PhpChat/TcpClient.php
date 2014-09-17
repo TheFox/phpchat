70631f6d2275fc122e54340d533520844fc92b05
Christian Mayer <christian@fox21.at>
2014-09-17 17:48:43 +0200

    Clean up.

--- src/TheFox/PhpChat/TcpClient.php
+++ src/TheFox/PhpChat/TcpClient.php
@@ -41,36 +41,27 @@ class TcpClient extends Client{
 	
 	private function checkPingSend(){
 		if($this->pingTime < time() - static::PING_TTL){
-			#print __CLASS__.'->'.__FUNCTION__.''."\n";
-			
 			$this->sendPing();
 		}
 	}
 	
 	private function checkPongTimeout(){
 		if(!$this->pongTime){
-			#print __CLASS__.'->'.__FUNCTION__.': set pong time'."\n";
 			$this->pongTime = time();
 		}
-		#print __CLASS__.'->'.__FUNCTION__.': check '.( time() - static::PONG_TTL - $this->pongTime )."\n";
 		if($this->pongTime < time() - static::PONG_TTL){
-			#print __CLASS__.'->'.__FUNCTION__.': shutdown'."\n";
 			$this->sendQuit();
 			$this->shutdown();
 		}
 	}
 	
 	public function dataRecv($data = null){
-		#fwrite(STDOUT, $this->getId().' dataRecv'."\n");
-		
 		$dataRecvReturnValue = '';
 		if($data === null && $this->getSocket()){
 			$data = $this->getSocket()->read();
 		}
 		
 		$this->incTrafficIn(strlen($data));
-		#fwrite(STDOUT, $this->getId().' dataRecv data: /'.$data.'/'."\n");
-		#fwrite(STDOUT, $this->getId().'  > len: '.strlen($data).''."\n");
 		
 		do{
 			$separatorPos = strpos($data, static::MSG_SEPARATOR);
@@ -83,36 +74,20 @@ class TcpClient extends Client{
 				$this->recvBufferTmp = '';
 				
 				$msg = base64_decode($msg);
-				#fwrite(STDOUT, $this->getId().' dataRecv msg: /'.$msg.'/'."\n");
 				
-				#$dataRecvReturnValue .= $this->msgHandle($msg);
 				$msgHandleReturnValue = $this->msgHandleRaw($msg);
 				$dataRecvReturnValue .= $msgHandleReturnValue;
-				#$dataRecvReturnValue = array_merge($dataRecvReturnValue, $msgHandleReturnValue);
-				#fwrite(STDOUT, 'msgHandle rv:         /'.$msgHandleReturnValue.'/'."\n");
-				#fwrite(STDOUT, 'msgHandle rv:         /'.base64_decode($msgHandleReturnValue).'/'."\n");
 				
 				$data = substr($data, $separatorPos + 1);
 			}
 		}
 		while($data);
 		
-		#fwrite(STDOUT, 'dataRecv: /'.base64_decode($dataRecvReturnValue).'/'."\n");
-		#fwrite(STDOUT, 'dataRecv rv: /'.$dataRecvReturnValue.'/'."\n");
-		#fwrite(STDOUT, 'dataRecv rv: /'.preg_split('/\n/', substr($dataRecvReturnValue, 0, -1)).'/'."\n");
-		
 		return $dataRecvReturnValue;
-		#return join(static::MSG_SEPARATOR, $dataRecvReturnValue);
-		#return join(',', $dataRecvReturnValue);
 	}
 	
 	public function dataSend($data){
-		#fwrite(STDOUT, $this->getId().' dataSend'."\n");
-		#fwrite(STDOUT, $this->getId().' dataSend data: /'.$data.'/'."\n");
-		#fwrite(STDOUT, $this->getId().' <  len: '.strlen($data).''."\n");
-		
 		$msg = '';
-		
 		if($data){
 			$data = base64_encode($data);
 			$this->incTrafficOut(strlen($data) + static::MSG_SEPARATOR_LEN);
@@ -121,7 +96,6 @@ class TcpClient extends Client{
 				$this->getSocket()->write($msg);
 			}
 		}
-		
 		return $msg;
 	}
 	
