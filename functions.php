6ce18cf09f7b8014642214c6b832ed9f7a078ec5
Christian Mayer <christian@fox21.at>
2014-10-14 13:32:40 +0200

    ve() removed.

--- functions.php
+++ functions.php
@@ -1,36 +1,5 @@
 <?php
 
-function ve($v = null){
-	try{
-		$rv = var_export($v, true);
-		#print "\n";
-		fwrite(STDOUT, $rv."\n");
-	}
-	catch(Exception $e){
-		print 'ERROR: '.$e->getMessage()."\n";
-	}
-}
-
-function vej($v = null){
-	try{
-		ve(json_encode($v));
-	}
-	catch(Exception $e){
-		print 'ERROR: '.$e->getMessage()."\n";
-	}
-}
-
-function vew($v = null){
-	try{
-		print '<pre>';
-		var_export($v);
-		print '</pre>';
-	}
-	catch(Exception $e){
-		print 'ERROR: '.$e->getMessage()."\n";
-	}
-}
-
 function strIsIp($ip){
 	if(preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $ip)){
 		return true;
